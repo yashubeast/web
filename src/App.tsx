34c8889bef8483f592f42a98c15af0e6b9554b1d
yashubeast <shenziouminami@gmail.com>
2025-11-28 02:51:22 +0530

    navbar shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import CompNavbar from './comps/navbar.tsx'
-import CompFooter from './comps/footer.tsx'
 
 import PageHome from './pages/home.tsx'
 import PageBlog from './pages/blog.tsx'
@@ -14,17 +13,13 @@ function App() {
 
       <CompNavbar/>
 
-      <div className='body'>
+      <div className='container'>
       <Routes>
-
         <Route path="/" element=<PageHome/> />
         <Route path="/blog" element=<PageBlog/> />
         <Route path="/about" element=<PageAbout/> />
-
       </Routes>
       </div>
-
-      {/* <CompFooter/> */}
 
     </BrowserRouter>
   )
--- src/comps/navbar.tsx
+++ src/comps/navbar.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FiGithub } from 'react-icons/fi'
+import React, { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+// import { FiGithub } from 'react-icons/fi'
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
+import './navbar.css'
 
 const navLinks = [
   { title: 'Home', href: '/' },
@@ -8,52 +11,82 @@ const navLinks = [
   { title: 'About', href: '/about' },
 ]
 
-const extLinks = [
-  { title: 'github', icon: FiGithub, href: 'https://github.com/yashubeast' },
-]
+// const extLinks = [
+//   { title: 'github', icon: FiGithub, href: 'https://github.com/yashubeast' },
+// ]
+
+// NOTE: add inert so u can't tab into the nav buttons while its off the screen aka hidden
+
 
 const CompNavbar: React.FC = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
+  const location = useLocation()
+
+  const handleOpen = () => setIsOpen(true)
+  const handleClose = () => setIsOpen(false)
+
   return (
-    <nav className="navbar">
-      <div className="flex justify-between items-center max-w-9xl mx-auto px-10 py-4">
+    <>
+    <button className='h-14 w-14 nav-open flex justify-center items-center g-button'
+        onClick={handleOpen}
+      >
+      <IoMenu className='w-6 h-6'/>
+    </button>
+
+    <nav className={`flex justify-between items-center w-full ${isOpen ? 'show' : ''}`}>
+      {/* <div className="flex justify-between items-center max-w-9xl mx-auto px-10 py-4"> */}
         
-        <div className="text-4xl font-semibold w-1/3 text-left fg-purple">
-          web
+        <div className="title flex justify-between">
+          <p className='p-[1em_2em] g-button '>Universium</p>
+          <button className='w-14 h-14 nav-close flex justify-center items-center g-button '
+            onClick={handleClose}
+          >
+          <IoClose className='w-7 h-7'/>
+          </button>
         </div>
 
-        <div className="flex justify-center items-center w-1/3 space-x-10 text-2xl">
-          {navLinks.map((link) => (
+        <ul className="list-none flex">
+          {navLinks.map((link) => {
+            const isActive = location.pathname === link.href
+            const baseClasses = 'p-[1em_2em] g-button cursor-pointer'
+            const activeClasses = 'fg-purple font-bold border-b-2 border-purple'
+            return (
             <Link
               key={link.title}
               to={link.href}
-              className="fg-purple"
+              className={isActive ? `${baseClasses} ${activeClasses}` : baseClasses}
             >
               {link.title}
             </Link>
-          ))}
-        </div>
+          )})}
+        </ul>
 
-        <div className='flex items-center space-x-5 w-1/3 justify-end'>
-          {extLinks.map((link) => (
-            <a
-              key={link.title}
-              href={link.href}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={link.title}
-              className="fg-purple"
-            >
-              {link.icon ? (
-                <link.icon size={28} />
-              ) : (
-                link.title
-              )}
-            </a>
-          ))}
-        </div>
+        {/* <div className=''> */}
+        {/*   {extLinks.map((link) => ( */}
+        {/*     <a */}
+        {/*       key={link.title} */}
+        {/*       href={link.href} */}
+        {/*       target="_blank" */}
+        {/*       rel="noopener noreferrer" */}
+        {/*       aria-label={link.title} */}
+        {/*       className="fg-purple" */}
+        {/*     > */}
+        {/*       {link.icon ? ( */}
+        {/*         <link.icon size={28} /> */}
+        {/*       ) : ( */}
+        {/*         link.title */}
+        {/*       )} */}
+        {/*     </a> */}
+        {/*   ))} */}
+        {/* </div> */}
 
-      </div>
+      {/* </div> */}
     </nav>
+
+    <div className='nav-overlay fixed inset-0' onClick={handleClose}>
+    </div>
+  </>
   )
 }
 
